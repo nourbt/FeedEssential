898b591ad83e1aa9f8fde311d1e53ee3d6f7f03f
nour.bteddini <nour.bteddini@gotocme.com>
2023-02-09 21:30:17 +0200

    Initial Commit

---
+++ FeedEssential/FeedEssential.h
@@ -0,0 +1,18 @@
+//
+//  FeedEssential.h
+//  FeedEssential
+//
+//  Created by Nour on 09/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeedEssential.
+FOUNDATION_EXPORT double FeedEssentialVersionNumber;
+
+//! Project version string for FeedEssential.
+FOUNDATION_EXPORT const unsigned char FeedEssentialVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedEssential/PublicHeader.h>
+
+
