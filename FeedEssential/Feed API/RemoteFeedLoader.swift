97e3d8bea5022f2798c090e93d85bb4d62b16c25
nour.bteddini <nour.bteddini@gotocme.com>
2023-02-12 21:17:31 +0200

    Delivers connectivity error on client

--- FeedEssential/Feed API/RemoteFeedLoader.swift
+++ FeedEssential/Feed API/RemoteFeedLoader.swift
@@ -9,19 +9,27 @@ import Foundation
 
 // HTTPClient can be public as it could be implemented by external modules
 public protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error) -> Void)
 }
 
 public final class RemoteFeedLoader {
     private let url: URL
     private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case connectivity
+    }
     // can be created by external modules, so make it public
    public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
-
-    public func load() { // within our method we invoke somthng like HTTPClient..
-        client.get(from: url)
+    // within our method we invoke somthng like HTTPClient..
+    // default closure
+    public func load(completion: @escaping (Error) -> Void = { _ in }) {
+        // mapping between our Error and the domain Error connectivity
+        client.get(from: url) { error in
+            completion(.connectivity)
+        }
     }
 }
