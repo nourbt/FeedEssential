c43b83765fe5d97fc85c462159bff42d1099e799
nour.bteddini <nour.bteddini@gotocme.com>
2023-03-07 22:13:48 +0200

    Delivers invalidData on non 200 HTTPResponse

--- FeedEssential/Feed API/RemoteFeedLoader.swift
+++ FeedEssential/Feed API/RemoteFeedLoader.swift
@@ -9,7 +9,7 @@ import Foundation
 
 // HTTPClient can be public as it could be implemented by external modules
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error) -> Void)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
 }
 
 public final class RemoteFeedLoader {
@@ -18,6 +18,7 @@ public final class RemoteFeedLoader {
     
     public enum Error: Swift.Error {
         case connectivity
+        case invalidData
     }
     // can be created by external modules, so make it public
    public init(url: URL, client: HTTPClient) {
@@ -26,10 +27,15 @@ public final class RemoteFeedLoader {
     }
     // within our method we invoke somthng like HTTPClient..
     // default closure
-    public func load(completion: @escaping (Error) -> Void = { _ in } ) {
+    public func load(completion: @escaping (Error) -> Void) {
         // mapping between our Error and the domain Error connectivity
-        client.get(from: url) { error in
-            completion(.connectivity)
+        client.get(from: url) { error, response in
+            if response != nil {
+                completion(.invalidData)
+            } else if error != nil {
+                completion(.connectivity)
+            }
+            
         }
     }
 }
--- FeedEssentialTests/RemoteFeedLoaderTests.swift
+++ FeedEssentialTests/RemoteFeedLoaderTests.swift
@@ -20,7 +20,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(string: "https://a-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.load()  // Act pass as param
+        sut.load { _ in }  // Act pass as param
         XCTAssertEqual(client.requestedURLs, [url]) // Assert
     }
     
@@ -28,8 +28,8 @@ class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(string: "https://a-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.load() // Act pass as param
-        sut.load()
+        sut.load { _ in } // Act pass as param
+        sut.load { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url, url]) // Assert
     }
@@ -48,6 +48,20 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(capturedError, [.connectivity])
     }
     
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+    
+        
+        var capturedError = [RemoteFeedLoader.Error]()
+        sut.load(){
+            capturedError.append($0) // stubbing
+            
+        }
+        let clientError = NSError(domain: "Test", code: 0)
+        client.complete(withStatusCode: 400)
+        XCTAssertEqual(capturedError, [.invalidData])
+    }
+    
     // MARK: -Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -59,19 +73,25 @@ class RemoteFeedLoaderTests: XCTestCase {
     // is capturing the URLs passed to the HTTPClient in requestedURLs property
     class HTTPClientSpy: HTTPClient {
     
-        private var messages = [(url: URL, completion: (Error) -> Void)]()
+        private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
         // computed property
         var requestedURLs: [URL] {
             return messages.map {$0.url}
         }
 
-        func get(from url: URL, completion: @escaping (Error) -> Void) {
+        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
             //clients only captures values
             messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error)
+            messages[index].completion(error, nil)
+        }
+        
+        func complete(withStatusCode code: Int, at index: Int = 0) {
+            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)
+            
+            messages[index].completion(nil, response)
         }
 
     }
