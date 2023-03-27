6918e51fc63981651d144e85af541d9d6fd556b7
nour.bteddini <nour.bteddini@gotocme.com>
2023-03-27 22:58:15 +0300

    Delivers items array on 200 HTTPResponse with JSONItems

--- FeedEssential/Feed Feature/FeedItem.swift
+++ FeedEssential/Feed Feature/FeedItem.swift
@@ -8,8 +8,26 @@
 import Foundation
 
 public struct FeedItem: Equatable{
-    let id: UUID
-    let description: String?
-    let location: String?
-    let imageURL: URL
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL){
+         self.id = id
+         self.description = description
+         self.location = location
+         self.imageURL = imageURL
+    }
+}
+
+// we need to provide some mapping instructions since FeedItem has imageURL while the json in response has image
+extension FeedItem: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case description
+        case location
+        case imageURL = "image"
+    }
 }
